27c01aa4b05f0c6ba553020c42a57b6015575b40
HridanshuR <90255356+HridanshuR@users.noreply.github.com>
2023-03-16 21:45:44 +0530

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,27 @@
+#quiz {
+    width: 500px;
+    margin: 0 auto;
+    text-align: center;
+}
+
+#question {
+    font-size: 20px;
+    margin-bottom: 20px;
+}
+
+#choices {
+    margin-bottom: 20px;
+}
+
+#choices button {
+    margin-right: 10px;
+}
+
+#submit {
+    margin-bottom: 20px;
+}
+
+#scoreboard {
+    font-size: 20px;
+    margin-bottom: 20px;
+}
